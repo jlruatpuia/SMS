aa48c801f55a6abf7c0c6c3fffa042c437701712
Ruatpuia JL <jlruatpuia@outlook.com>
2016-01-11 13:59:32 +0530

    Customer Reference

---
+++ SMS.Win/UserControls/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using SMS.DAL;
+
+namespace SMS.Win.UserControls
+{
+    public partial class Customer : DevExpress.XtraEditors.XtraUserControl
+    {
+        Customers cus = new Customers();
+
+        public Customer()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
